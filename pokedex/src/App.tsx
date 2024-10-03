35b30b0ac30505b69c4144752a84f762a963bad2
Moisés Rodrigues Martins <moisesmartins.box@gmail.com>
2024-10-03 20:58:42 -0300

    Parte1: Configurações Iniciais e Criação do Setup

---
+++ pokedex/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="w-full flex flex-col">
+      <h1 className="text-blue-600 font-semibold text-2xl">Hello World</h1>
+    </div>
+  );
+}
